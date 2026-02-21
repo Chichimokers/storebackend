62efc200eb700e96479f93c3d8cb7e91764daf1b
Chichimokers <developer1575@gmail.com>
2026-02-21 14:01:24 -0500

    fix deploy

--- config/settings.py
+++ config/settings.py
@@ -134,14 +134,14 @@ SIMPLE_JWT = {
 CORS_ALLOW_ALL_ORIGINS = DEBUG
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:3000,http://127.0.0.1:3000',
+    default='http://localhost:3000,http://127.0.0.1:3000,inventory.cloudns.be',
     cast=Csv()
 )
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = config(
     'CSRF_TRUSTED_ORIGINS',
-    default='http://localhost:8000,http://127.0.0.1:8000',
+    default='http://localhost:8000,http://127.0.0.1:8000,inventory.cloudns.be',
     cast=Csv()
 )
 
